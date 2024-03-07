8e9b23bafeba96205c328b826c94257c14e5db63
Ahmedsaleh1234 <a12661826@gmail.com>
2024-03-07 19:08:23 +0200

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
