91a6b44eec257419039e8a7a9914311ee70389b9
Ahmedsaleh1234 <a12661826@gmail.com>
2024-03-12 14:48:47 +0200

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+const getListStudents = () => [
+  {
+    id: 1,
+    fristname: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    fristname: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    fristname: 'Serena',
+    location: 'San Francisco',
+
+  },
+];
+export default getListStudents;
